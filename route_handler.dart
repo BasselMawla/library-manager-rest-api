7e2275855165a8aa287a29a3ddfcdd68bbf5f594
Bassel Ballout El Mawla <balloutelb@coventry.ac.uk>
2022-03-08 17:09:01 +0000

    Added students route controller

--- route_handler.dart
+++ route_handler.dart
@@ -3,27 +3,18 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import 'routes/students_controller.dart';
+
 // Routing setup
 class RouteHandler {
   Handler get handler {
     final router = Router();
 
-    // GET test route
-    router.get("/books", (Request request) {
-      return Response.ok("Book returned");
-    });
-
-    // POST test route
-    router.post("/students", (Request request) async {
-      final body = await request.readAsString();
-      print(body);
-      return Response.ok(body);
-    });
+    router.mount("/students", StudentsController().router);
 
     // All invalid URLs
-    router.all("/<ignored|.*>", (Request request) {
-      return Response.notFound("Page Not Found!");
-    });
+    router.all("/<ignored|.*>",
+        (Request request) => Response.notFound("Page Not Found!"));
 
     return router;
   }
